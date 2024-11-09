97e1b0dba276c9ffadd18c08105428cabbeb9505
klenkes74 <rlichti@kaiserpfalz-edv.de>
2024-11-09 20:55:42 +0100

    WIP. Moving to my laptop on the couch ...

--- src/main/java/de/paladinsinn/tp/dcis/users/Application.java
+++ src/main/java/de/paladinsinn/tp/dcis/users/Application.java
@@ -1,14 +1,12 @@
 package de.paladinsinn.tp.dcis.users;
 
+import de.paladinsinn.tp.dcis.commons.configuration.EnableDefaultConfiguration;
+import de.paladinsinn.tp.dcis.commons.configuration.EnableMessagingConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
-import jakarta.servlet.DispatcherType;
 import lombok.Getter;
 
 /**
@@ -17,6 +15,8 @@ import lombok.Getter;
  * @since 2024-06-09
  */
 @SpringBootApplication
+@EnableDefaultConfiguration
+@EnableMessagingConfiguration
 public class Application extends SpringApplication {
     @Value("${spring.application.name:PLAYERS}")
     @Getter(onMethod = @__(@Bean))
@@ -26,12 +26,4 @@ public class Application extends SpringApplication {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
-        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
-        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
-        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registration;
-    }
 }

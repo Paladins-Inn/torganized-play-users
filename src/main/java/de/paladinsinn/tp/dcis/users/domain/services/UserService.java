beb6cd85e9a147ca4e8e074c50c10491d3bb379a
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-11-30 11:58:31 +0100

    Added more XSlf4j based logging.

--- src/main/java/de/paladinsinn/tp/dcis/users/domain/services/UserService.java
+++ src/main/java/de/paladinsinn/tp/dcis/users/domain/services/UserService.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2024. Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package de.paladinsinn.tp.dcis.users.domain.services;
 
 import java.util.LinkedList;
@@ -5,6 +23,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -18,12 +37,11 @@ import de.paladinsinn.tp.dcis.users.domain.persistence.UserRepository;
 import de.paladinsinn.tp.dcis.users.domain.persistence.UserToJpa;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @ToString(callSuper = true, onlyExplicitlyIncluded = true)
-@Slf4j
+@XSlf4j
 public class UserService {
     private final UserRepository playerRepository;
 
@@ -35,12 +53,13 @@ public class UserService {
     private final String applicationName;
 
     public User createUser(User player) {
+        log.entry(player);
+
         player = playerRepository.save(toUserJPA.apply(player));
 
         logService.log(player.getId(), applicationName, "player.created");
 
-        log.info("Created player in database. player={}", player);
-        return player;
+        return log.exit(player);
     }
 
     public User createUser(final UUID uid, final String nameSpace, final String name) {
@@ -53,26 +72,32 @@ public class UserService {
     }
 
     public Optional<User> retrieveUser(final UUID uid) {
+        log.entry(uid);
+
         Optional<UserJPA> result = playerRepository.findById(uid);
 
         log.debug("Loaded player from database. uid={}, player={}", uid, result.isPresent() ? result.get() : "***none***");
-        return Optional.ofNullable(result.isPresent() ? toUser.apply(result.get()) : null);
+
+        return Optional.of(log.exit(result.orElse(null)));
     }
 
     public List<User> retrieveUsers(final String nameSpace) {
-        List<User> result = new LinkedList<>(playerRepository.findByNameSpace(nameSpace).stream().map(toUser::apply).toList());
+        log.entry(nameSpace);
 
-        log.debug("Loaded users for namespace. nameSpace='{}', users={}", nameSpace, result);
-        return result;
+        List<User> result = new LinkedList<>(playerRepository.findByNameSpace(nameSpace).stream().map(toUser).toList());
+
+        return log.exit(result);
     }
 
     public Page<User> retrieveUsers(final String nameSpace, final Pageable pageable) {
+        log.entry(nameSpace, pageable);
+
         Page<UserJPA> data = playerRepository.findByNameSpace(nameSpace, pageable);
         Page<User> result = new PageImpl<>(new LinkedList<>(data.stream().map(toUser::apply).toList()), pageable, data.getTotalElements());
 
         log.debug("Loaded users for namespace. nameSpace='{}', page={}/{}, size={}", nameSpace, 
                 result.getPageable().getPageNumber(), result.getTotalPages(), result.getTotalElements());
-        return result;
-    }
 
+        return log.exit(result);
+    }
 }

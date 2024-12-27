01d18362bf299210ccb4460f0a07c50edc0aa17f
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-12-27 09:02:22 +0100

    added a bit of logging to analyse ...

--- src/main/java/de/paladinsinn/tp/dcis/users/controller/PrivateMainController.java
+++ src/main/java/de/paladinsinn/tp/dcis/users/controller/PrivateMainController.java
@@ -1,6 +1,7 @@
 package de.paladinsinn.tp.dcis.users.controller;
 
 import lombok.extern.slf4j.XSlf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +15,27 @@ import jakarta.annotation.security.RolesAllowed;
 @XSlf4j
 public class PrivateMainController {
 
+
+
     @GetMapping
     @RolesAllowed({"ADMIN", "ORGA", "JUDGE", "GM", "PLAYER"})
-    public String index(Model model) {
-        log.entry(model);
+    public String index(final Authentication authentication, Model model) {
+        log.entry(authentication, model);
+
+        logAuthorities(authentication);
 
         model.addAttribute("access_level", "Spieler");
 
         String result = "main";
-        log.exit(result);
-        return result;
+        return log.exit(result);
+    }
+
+    private void logAuthorities(Authentication authentication) {
+        log.entry(authentication);
+
+        authentication.getAuthorities().forEach(authority -> log.debug("Authority: {}", authority));
+
+        log.exit();
     }
 
     @GetMapping(path = "/settings")
@@ -34,8 +46,7 @@ public class PrivateMainController {
         model.addAttribute("access_level", "Spieler");
 
         String result = "not-implemented-yet";
-        log.exit(result);
-        return result;
+        return log.exit(result);
     }
 
 }
